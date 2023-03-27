ef2dfc5b27372e4fc17c3892733431dd7a5dfb09
Igors Saters <s21sateigor@venta.lv>
2023-03-27 11:16:34 +0300

    Add: MyVerticeNode class

---
+++ src/MyVerticeNode.java
@@ -0,0 +1,26 @@
+public class MyVerticeNode<T> {
+    private T element;
+    private MyEdgeNode firstEdge = null;
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T inputElement) {
+        if(inputElement != null){
+            element = inputElement;
+        } else {
+            element = (T) new Object();
+        }
+    }
+
+    public MyEdgeNode getFirstEdge() {
+        return firstEdge;
+    }
+
+    public void setFirstEdge(MyEdgeNode firstEdge) {
+        this.firstEdge = firstEdge;
+    }
+
+
+}
